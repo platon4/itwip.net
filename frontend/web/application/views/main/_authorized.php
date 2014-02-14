45eeb6d65b51cff33e56c1c59f3e56b684cd2585
eolitich <eolitich@gmail.com>
2014-02-14 21:26:02 +0200

    Christmas tree removal

--- frontend/web/application/views/main/_authorized.php
+++ frontend/web/application/views/main/_authorized.php
@@ -10,29 +10,6 @@ $this->metaDescription=Yii::t('main','_indexUser_Description');
 
 <div class="content_right_v">
     <div class="content_right_l"> 
-        <div class="block" style="width: 100%; margin-bottom: 20px;">
-            <div class="block_title"><div class="block_title_inset"><h5>Поздравляем с наступающим новым годом!</h5></div></div>
-            <div class="block_content"  style="padding: 10px 0px;position: relative">
-                <a href="/" style="display: block;width: 100%;height: 100%;position: absolute;top: 0;left: 0;"></a>
-                <div class="table">
-                    <div class="td" style="width: 180px">
-                        <object type="application/x-shockwave-flash" data="http://xflash.ucoz.ru/slider/620.swf" width="170" height="200">
-                            <param name="bgcolor" value="#ffffff" />
-                            <param name="allowFullScreen" value="true" />
-                            <param name="allowScriptAccess" value="always" />
-                            <param name="wmode" value="transparent" />
-                            <param name="movie" value="" />  
-                            <param name="flashvars" value="st=;file=" />
-                            <param name="link" value="false" />
-                        </object>                     
-                    </div>
-                    <div class="td" style="padding-top: 25px;">
-                        Для всех без исключения пользователей, мы подготовили небольшой но довольно приятный подарок!<br /><br /> До 21 января 2014г. Ваш доход будет 70% с приглашённого Вами реферала! <br /><br />От всей нашей дружной команды, желаем Вам успехов и больших заработков в новом году !!!
-                    </div>
-                </div>
-               
-            </div>
-        </div> 
     <?php $this->widget('application.widgets.Messages'); ?>    
     </div>
     <div class="content_right_r">
