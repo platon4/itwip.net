8f6638dfe87a3e90c32f70b4ee66fdfec186e5c4
eolitich <eolitich@gmail.com>
2014-02-14 20:56:06 +0200

    Add vendor dir

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,24 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+);
