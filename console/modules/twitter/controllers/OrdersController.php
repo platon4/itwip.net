eafc65bab54d6cc08226424cb40e4a8cc2195302
eolitich <eolitich@gmail.com>
2014-02-15 20:22:04 +0200

    Fixed errors & add new files

---
+++ console/modules/twitter/controllers/OrdersController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eolitich
+ * Date: 15.02.14
+ * Time: 20:08
+ */
+
+namespace console\modules\twitter\controllers;
+
+
+class OrdersController {
+
+} 
--- console/modules/twitter/controllers/TweetingController.php
+++ console/modules/twitter/controllers/TweetingController.php
@@ -4,10 +4,10 @@ namespace console\modules\twitter\controllers;
 
 use Yii;
 
-class DefaultController extends \yii\web\Controller
+class DefaultController extends \console\components\Controller
 {
 	public function actionIndex()
 	{
-
+		echo 51;
 	}
 } 
