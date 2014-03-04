fb1b9fcc0b000d3b5731e47ac2f1c1fe3824a23b
eolitich <eolitich@gmail.com>
2014-03-04 18:12:29 +0200

    fix runtime

--- console/config/main.php
+++ console/config/main.php
@@ -2,33 +2,34 @@
 $rootDir = __DIR__ . '/../..';
 
 $params = array_merge(
-	require($rootDir . '/common/config/params.php'),
-	require($rootDir . '/common/config/params-local.php'),
-	require(__DIR__ . '/params.php'),
-	require(__DIR__ . '/params-local.php')
+    require($rootDir . '/common/config/params.php'),
+    require($rootDir . '/common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
 );
 
 return [
-	'id' => 'console',
-	'basePath' => dirname(__DIR__),
-	'modules' => [
-		'twitter' => 'console\modules\twitter\Twitter',
-        'cleaner' => 'console\modules\cleaner\Cleaner'
-	],
-	'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
-	'components' => [
-		'db' => $params['components.db'],
-		'cache' => $params['components.cache'],
-		'mail' => $params['components.mail'],
-		'redis' => $params['components.redis'],
-		'log' => [
-			'targets' => [
-				[
-					'class' => 'yii\log\FileTarget',
-					'levels' => ['error', 'warning'],
-				],
-			],
-		],
-	],
-	'params' => $params,
+    'id'         => 'console',
+    'basePath'   => dirname(__DIR__),
+    'modules'    => [
+        'twitter' => 'console\modules\twitter\Twitter',
+        'cleaner' => 'console\modules\cleaner\Cleaner',
+        'finance' => 'console\modules\cleaner\Finance'
+    ],
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'components' => [
+        'db'    => $params['components.db'],
+        'cache' => $params['components.cache'],
+        'mail'  => $params['components.mail'],
+        'redis' => $params['components.redis'],
+        'log'   => [
+            'targets' => [
+                [
+                    'class'  => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params'     => $params,
 ];
---
+++ console/modules/finance/Finance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace console\modules\finance;
+
+use Yii;
+use yii\base\Module;
+
+class Finance extends Module
+{
+
+}
