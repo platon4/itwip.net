7f28b6f9aff827e3b8057c3f3777d0302c2e6090
eolitich <eolitich@gmail.com>
2014-03-12 15:34:20 +0200

    ....

--- frontend/views/main/info.php
+++ frontend/views/main/info.php
@@ -11,12 +11,12 @@
     	<div id="modal_info">
     		<div class="title_modal_info"><?php echo Html::encode($title); ?></div>
     		<div class="content_modal_info">
-    		<?php if($is_html) { ?>
+    		<?php if(isset($is_html) && $is_html) { ?>
     			<?php echo $message; ?>
     		<?php } else { ?>
     			<?php echo Html::bbCode($message); ?>
     		<?php } ?>
-    		<?php if($link) { ?>
+    		<?php if(isset($link) && $link) { ?>
     			<div style="text-align: center; margin-top: 10px;"><a href="<?php echo $link; ?>"><?php echo (isset($link_screen) && $link_screen) ? $link_screen : Yii::t('main', '_back'); ?></a></div>
     		<?php } ?>
     		</div>
