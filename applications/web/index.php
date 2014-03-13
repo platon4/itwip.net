b31d6de5cf7f2fcbdb1f49a8707ca2a37cb3fd39
eolitich <eolitich@gmail.com>
2014-03-13 08:05:39 +0200

    test

--- applications/web/index.php
+++ applications/web/index.php
@@ -1,1 +1,21 @@
 <?php
+/*
+=====================================================
+ Copyright (c) iTwip.net 2013
+=====================================================
+*/
+
+date_default_timezone_set('Europe/Moscow');
+
+define('YII_DEBUG', false);
+
+require_once(dirname(__DIR__) . '/framework/yii.php');
+require_once(dirname(__DIR__) . '/config/aliases.php');
+require_once(dirname(__DIR__) . '/components/ArrayHelper.php');
+
+$config = ArrayHelper::merge(
+    require(dirname(__DIR__) . '/config/main.php'),
+    require(dirname(__DIR__) . '/config/main-local.php')
+);
+
+Yii::createWebApplication($config)->run();
