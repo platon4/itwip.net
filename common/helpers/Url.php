bf79a4bbe8dd82159e114a58f312f5e184753144
eolitich <eolitich@gmail.com>
2014-03-17 08:01:24 +0200

    ...

--- common/helpers/Url.php
+++ common/helpers/Url.php
@@ -21,4 +21,23 @@ class Url
 
         return $url;
     }
+
+    public static function getHostUrl()
+    {
+        if(self::$_hostInfo  === null) {
+            $secure = Yii::$app->request->getIsSecureConnection();
+            $http = $secure ? 'https' : 'http';
+            if(isset($_SERVER['HTTP_HOST'])) {
+                self::$_hostInfo = $http . '://' . $_SERVER['HTTP_HOST'];
+            } else {
+                self::$_hostInfo = $http . '://' . $_SERVER['SERVER_NAME'];
+                $port = $secure ? Yii::$app->request->getSecurePort() : Yii::$app->request->getPort();
+                if(($port !== 80 && !$secure) || ($port !== 443 && $secure)) {
+                    self::$_hostInfo .= ':' . $port;
+                }
+            }
+        }
+
+        return self::$_hostInfo;
+    }
 } 
