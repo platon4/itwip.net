e7de03e0c17641486f2530cab5ebbbf89ea17c13
eolitich <eolitich@gmail.com>
2014-03-23 15:41:35 +0200

    ...

--- console/modules/twitter/controllers/DefaultController.php
+++ console/modules/twitter/controllers/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace console\modules\twitter\controllers;
 
+use console\components\Logger;
 use Yii;
 use yii\db\Query;
 use common\helpers\String;
@@ -22,30 +23,36 @@ class DefaultController extends \console\components\Controller
      */
     protected function launchTweetingDaemons()
     {
-        Yii::$app->redis->set('console:twitter:tweeting', 'true', 30);
+        if(Yii::$app->redis->exists('console:twitter:tweeting:isRun') === false) {
+            Yii::$app->redis->set('console:twitter:tweeting', 'true', 30);
 
-        sleep(rand(35, 60));
+            sleep(rand(35, 60));
 
-        $rows = (new Query())
-            ->select('daemon')
-            ->from('{{%twitter_tweeting}}')
-            ->groupBy(['daemon'])
-            ->all();
+            $rows = (new Query())
+                ->select('daemon')
+                ->from('{{%twitter_tweeting}}')
+                ->groupBy(['daemon'])
+                ->all();
 
-        if(!empty($rows)) {
-            $path = realpath(Yii::$app->getBasePath() . '/..');
-            $runtime = Yii::$app->getRuntimePath() . "/daemon";
+            if(!empty($rows)) {
+                $path = realpath(Yii::$app->getBasePath() . '/..');
+                $runtime = Yii::$app->getRuntimePath() . "/daemon";
 
-            if(!is_dir($runtime)) {
-                @mkdir($runtime, 0777);
-                @chmod($runtime, 0777);
-            }
+                if(!is_dir($runtime)) {
+                    @mkdir($runtime, 0777);
+                    @chmod($runtime, 0777);
+                }
 
-            foreach($rows as $row) {
-                $daemon = $row['daemon'];
-                $log = $runtime . "/daemon_log_" . $daemon . ".txt";
+                foreach($rows as $row) {
+                    $daemon = $row['daemon'];
+                    $log = $runtime . "/daemon_log_" . $daemon . ".txt";
 
-                exec("nohup php $path/cmd twitter/tweeting " . $daemon . " > $log 2>&1 &");
+                    exec("nohup php $path/cmd twitter/tweeting " . $daemon . " > $log 2>&1 &");
+                }
+
+                Yii::$app->redis->set('console:twitter:tweeting:isRun', time(), 27 * 60);
+            } else {
+                Logger::log('Not tasks', 'daemons/run', 'tweetingDaemons');
             }
         }
     }
