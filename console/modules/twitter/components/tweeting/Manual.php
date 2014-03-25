f69645252bc5a469f3fd33605e80d41c71d99af1
eolitich <eolitich@gmail.com>
2014-03-25 08:39:41 +0200

    ...

--- console/modules/twitter/components/tweeting/Manual.php
+++ console/modules/twitter/components/tweeting/Manual.php
@@ -140,7 +140,7 @@ class Manual implements TweetingInterface
     public function getAccount($key, $all = false)
     {
         if($this->_account === null) {
-            $this->_account = (new Accounts())->where(['id' => $this->get('account')])->one();
+            $this->_account = (new Accounts())->where(['id' => $this->getParams('account')])->one();
         }
 
         if($all === true)
