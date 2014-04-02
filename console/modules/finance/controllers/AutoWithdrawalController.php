87c52710e670883cf540b21a29727237d6d0496a
eolitich <eolitich@gmail.com>
2014-04-02 13:43:13 +0300

    фикс вывод средств

--- console/modules/finance/controllers/AutoWithdrawalController.php
+++ console/modules/finance/controllers/AutoWithdrawalController.php
@@ -31,39 +31,39 @@ class AutoWithdrawalController extends \console\components\Controller
             $code = 0;
             $status = 3;
 
-            if($settings = unserialize($pay['_settings'])) {
-                $this->writeln('Settings init success');
+            try {
+                $this->writeln('Init transaction');
+                $t = Yii::$app->db->beginTransaction();
 
-                if(is_array($settings) AND isset($settings['purse']) AND trim($settings['purse']) != '') {
-                    $res = $wmxi->X2(
-                        $pay['id'],
-                        PRIMARY_PURSE, # номер кошелька с которого выполняется перевод (отправитель)
-                        $settings['purse'], # номер кошелька, но который выполняется перевод (получатель)
-                        round($pay['_out'], 2), # переводимая сумма
-                        0, # срок протекции сделки в днях
-                        '', # код протекции сделки
-                        'iTwip.net, вывод средств пользователю ' . $pay['email'] . '. Нам будет приятно если Вы оставите отзыв о сайте, перейдя по ссылке: http://advisor.wmtransfer.com/sitedetails.aspx?url=itwip.net', # описание оплачиваемого товара или услуги
-                        0, # номер счета (в системе WebMoney), по которому выполняется перевод
-                        1 # учитывать разрешение получателя
-                    );
+                if($settings = unserialize($pay['_settings'])) {
+                    $this->writeln('Settings init success');
 
-                    if($res->ErrorCode() == 0 || $res->ErrorCode() == 103) {
-                        $status = 2;
-                        $code = 200;
-                    } else {
-                        $code = $res->ErrorCode();
-                    }
+                    if(is_array($settings) AND isset($settings['purse']) AND trim($settings['purse']) != '') {
+                        $res = $wmxi->X2(
+                            $pay['id'],
+                            PRIMARY_PURSE, # номер кошелька с которого выполняется перевод (отправитель)
+                            $settings['purse'], # номер кошелька, но который выполняется перевод (получатель)
+                            round($pay['_out'], 2), # переводимая сумма
+                            0, # срок протекции сделки в днях
+                            '', # код протекции сделки
+                            'iTwip.net, вывод средств пользователю ' . $pay['email'] . '. Нам будет приятно если Вы оставите отзыв о сайте, перейдя по ссылке: http://advisor.wmtransfer.com/sitedetails.aspx?url=itwip.net', # описание оплачиваемого товара или услуги
+                            0, # номер счета (в системе WebMoney), по которому выполняется перевод
+                            1 # учитывать разрешение получателя
+                        );
 
-                    $this->writeln('Response code from webmoney ' . $res->ErrorCode());
+                        if($res->ErrorCode() == 0 || $res->ErrorCode() == 103) {
+                            $status = 2;
+                            $code = 200;
+                        } else {
+                            $code = $res->ErrorCode();
+                        }
 
-                    try {
-                        $this->writeln('Init transaction');
-                        $t = Yii::$app->db->beginTransaction();
-
-                        $this->updatePayOrder($pay, $status, $code);
+                        $this->writeln('Response code from webmoney ' . $res->ErrorCode());
 
                         if($code === 200) {
                             /** Реферальная система*/
+
+                            $this->updatePayOrder($pay, $status, $code);
 
                             $loyalty = (new Query())
                                 ->select('parent_referral as id,in_balance,loyalty_finance')
@@ -107,28 +107,20 @@ class AutoWithdrawalController extends \console\components\Controller
                         } else {
                             Logger::error('Error pay 1', array_merge($pay, ['code' => $code]), 'finance/errors', 'autoPayResponse');
                         }
-
-                        $t->commit();
-                    } catch(Exception $e) {
-                        $t->rollBack();
-                        Logger::error($e, array_merge($pay, ['code' => $code]), 'finance/errors', 'autoPayResponse');
+                    } else {
+                        Logger::error('Error not set purse', $pay, 'finance/errors', 'autoPay');
                     }
                 } else {
-                    try {
-                        $t = Yii::$app->db->beginTransaction();
-
-                        $this->updatePayOrder($pay, $status, $code);
-                        $t->commit();
-
-                    } catch(Exception $e) {
-                        $t->rollBack();
-                        Logger::error($e, array_merge($pay, ['code' => $code]), 'finance/errors', 'autoPayResponseNotPurse');
-                    }
-
-                    Logger::error('Error not set purse', $pay, 'finance/errors', 'autoPay');
+                    Logger::error('Error unserialize settings', $pay, 'finance/errors', 'autoPay');
                 }
-            } else {
-                Logger::error('Error unserialize settings', $pay, 'finance/errors', 'autoPay');
+
+                if($code !== 200)
+                    $this->cancelPay($pay, $status, $code);
+
+                $t->commit();
+            } catch(Exception $e) {
+                $t->rollBack();
+                Logger::error($e, array_merge($pay, ['code' => $code]), 'finance/errors', 'autoPayException');
             }
 
             sleep(3);
@@ -138,12 +130,26 @@ class AutoWithdrawalController extends \console\components\Controller
     public function updatePayOrder($pay, $status, $code)
     {
         $command = Yii::$app->db->createCommand();
+
         $rowCount = $command->update('{{%money_withdrawal}}', ['_status' => $status, '_date_execute' => date('Y-m-d H:i:s'), '_code' => $code], ['id' => $pay['id']])
             ->execute();
 
         if($rowCount)
             $command->delete('{{%money_blocking}}', ['_type' => '1', '_id' => $pay['id']])
                 ->execute();
+    }
+
+    public function cancelPay($pay, $status, $code)
+    {
+        Yii::$app->db->createCommand("UPDATE {{%accounts}} SET money_amount=money_amount+:money WHERE id=:id", [
+            ':id'     => $pay['owner_id'],
+            ':amount' => $pay['amount'],
+        ])
+            ->execute();
+
+        $this->updatePayOrder($pay, $status, $code);
+
+        Operation::log($pay['amount'], $pay['owner_id'], 0, 1, 1, 1, $pay['id'], '', 2);
     }
 
     public function writeln($msg)
