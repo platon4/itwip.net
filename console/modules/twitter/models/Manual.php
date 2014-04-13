b8b9d7a1e842f62f76510406f46b12cd3fa2570a
eolitich <eolitich@gmail.com>
2014-04-13 11:32:52 +0300

    ...

--- console/modules/twitter/models/Manual.php
+++ console/modules/twitter/models/Manual.php
@@ -32,10 +32,12 @@ class Manual extends Model
                 $status = $this->tweetStatus($row['tweet_id']);
 
                 if($status != 3) {
-                    echo $row['order_hash'].PHP_EOL;
+                    echo "Order hash: " . $row['order_hash'] . PHP_EOL;
 
                     $order_id = trim($row['order_hash']) != '' ? (new Query())->select('id')->from('{{%twitter_orders}}')->where(['order_hash' => $row['order_hash']])->scalar() : 0;
-echo $order_id.PHP_EOL;
+
+                    echo $order_id . PHP_EOL;
+
                     $pay_type = $row['payment_method'] == 1 ? 'bonus' : 'purse';
 
                     if(empty($row['screen_name']))
@@ -47,24 +49,26 @@ echo $order_id.PHP_EOL;
                         if($status === 0) {
                             Operation::unlockMoney($row['tweet_cost'], $row['return_amount'], $row['bloger_id'], $row['adv_id'], $pay_type, 'tweetsCheckSuccess', $row['tweet_id'], $order_id, $row['screen_name']);
 
-                            $command->update('{{%twitter_tweets}}', ['status' => 1], ['id' => $row['id']])->execute();
+                            $command->update('{{%twitter_tweets}}', ['status' => '1'], ['id' => $row['id']])->execute();
 
                             echo 'Success' . PHP_EOL;
+                            Logger::log($row, 'check-tweet', 'success-check-tweet');
                         } else {
                             Operation::cancelTransfer($row['tweet_cost'], $row['bloger_id'], $pay_type, 'tweetCheckUnsuccessfully', $row['tweet_id'], $row['screen_name']);
                             Operation::returnMoney($row['return_amount'], $row['adv_id'], $pay_type, 'bloggerDeletedTweet', $order_id, $order_id);
 
-                            $command->update('{{%twitter_tweets}}', ['status' => 2], ['id' => $row['id']])->execute();
+                            $command->update('{{%twitter_tweets}}', ['status' => '2'], ['id' => $row['id']])->execute();
 
                             if($status === 1)
                                 (new Accounts())->isSuspended($row['tw_account']);
 
                             echo 'Cancel' . PHP_EOL;
+                            Logger::log($row, 'check-tweet', 'error-check-tweet');
                         }
 
-                        print_r($row);
                         $t->commit();
                     } catch(Exception $e) {
+                        Logger::error($e, $row, 'check-tweet', 'exception-check-tweet');
                         $t->rollBack();
                     }
                 }
@@ -95,7 +99,7 @@ echo $order_id.PHP_EOL;
                 ->select('t.*,a.screen_name')
                 ->from('{{%twitter_tweets}} t')
                 ->leftJoin('{{%tw_accounts}} a', 't.tw_account=a.id')
-                ->where(['and', 't.status=0', 't.date<:date', 't.id=5684'], [':date' => date("Y-m-d H:i:s", time() - (3 * 86400))])
+                ->where(['and', 't.status=0', 't.date<:date'], [':date' => date("Y-m-d H:i:s", time() - (3 * 86400))])
                 ->orderBy(['t.date' => SORT_ASC])
                 ->limit(1)
                 ->all();
