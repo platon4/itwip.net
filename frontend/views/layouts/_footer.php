8995a2ec5b2464a2453af54bef52f039e29cb9c2
platon4 <vitsmolins@ya.ru>
2014-04-26 22:51:08 +0400

    новый код аналитики

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -84,7 +84,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-36434380-2', 'itwip.net');
+  ga('create', 'UA-45749223-1', 'itwip.net');
   ga('send', 'pageview');
 
 </script>
